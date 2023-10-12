f275841f2bb57358e049dca321986dc177222c04
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-10-12 14:27:19 +0500

    First Commit

---
+++ SkillFactory homework/SkillFactory homework/Program.cs
@@ -0,0 +1,31 @@
+﻿var phoneBook = new List<Contact>();
+
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+var newPhoneBook = phoneBook
+    .OrderBy(c => c.Name)
+    .ThenBy(c => c.LastName);
+
+foreach (var c in newPhoneBook)
+    Console.WriteLine($"{c.Name} {c.LastName}");
+
+public class Contact
+{
+    public Contact(string name, string lastName, long phoneNumber, string email)
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public string Name { get; }
+    public string LastName { get; }
+    public long PhoneNumber { get; }
+    public string Email { get; }
+}
